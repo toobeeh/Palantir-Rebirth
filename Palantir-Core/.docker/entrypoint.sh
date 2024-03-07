f02c62d660e6e462cc93b6037ebb33ddac587497
tobeh <tobiasch@gmx.at>
2024-03-08 00:41:11 +0100

    fix sed path

--- Palantir-Core/.docker/entrypoint.sh
+++ Palantir-Core/.docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-sed -i "s!DB_DOMAIN_NAME_SED_PLACEHOLDER!$GRPC_URL!g" /app/appsettings.json
-sed -i "s!DISCORD_TOKEN_SED_PLACEHOLDER!$DISCORD_TOKEN!g" /app/appsettings.json
-sed -i "s!PATREON_TOKEN_SED_PLACEHOLDER!$PATREON_TOKEN!g" /app/appsettings.json
+sed -i "s!DB_DOMAIN_NAME_SED_PLACEHOLDER!$GRPC_URL!g" /app/Configuration/appsettings.json
+sed -i "s!DISCORD_TOKEN_SED_PLACEHOLDER!$DISCORD_TOKEN!g" /app/Configuration/appsettings.json
+sed -i "s!PATREON_TOKEN_SED_PLACEHOLDER!$PATREON_TOKEN!g" /app/Configuration/appsettings.json
 
 # Start the main process
 exec "$@"
