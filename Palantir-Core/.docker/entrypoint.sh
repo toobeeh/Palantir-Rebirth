a1aa1b65c80cc63bea77dbda694faebb7ed994c8
tobeh <tobiasch@gmx.at>
2024-03-08 00:51:31 +0100

    fix grpc url in sed

--- Palantir-Core/.docker/entrypoint.sh
+++ Palantir-Core/.docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sed -i "s!DB_DOMAIN_NAME_SED_PLACEHOLDER!$GRPC_URL!g" /app/Configuration/appsettings.json
+sed -i "s!GRPC_URL_SED_PLACEHOLDER!$GRPC_URL!g" /app/Configuration/appsettings.json
 sed -i "s!DISCORD_TOKEN_SED_PLACEHOLDER!$DISCORD_TOKEN!g" /app/Configuration/appsettings.json
 sed -i "s!PATREON_TOKEN_SED_PLACEHOLDER!$PATREON_TOKEN!g" /app/Configuration/appsettings.json
 
