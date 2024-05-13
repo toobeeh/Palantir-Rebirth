dd1cfff8e300116d9f8115fd03518173a1286c9d
tobeh <tobiasch@gmx.at>
2024-05-13 09:12:16 +0200

    update event header

--- tobeh.Palantir.Lobbies/Util/LobbyMessageUtil.cs
+++ tobeh.Palantir.Lobbies/Util/LobbyMessageUtil.cs
@@ -116,9 +116,8 @@ public class LobbyMessageUtil
         var eventInfo = activeEvent is null
             ? ""
             : $"\n_ _```ansi\n{Formatter.Colorize($"{activeEvent.Name} Event", AnsiColor.Magenta)}\n```\n" +
-              $"From {Formatter.Timestamp(activeEvent.StartDate.ToDateTimeOffset())} - {Formatter.Timestamp(activeEvent.EndDate.ToDateTimeOffset())}\n" +
-              $"View your progress with `/event view {activeEvent.Id}`\n" +
-              $"`{activeEvent.Description}`\n_ _";
+              $"> Until {Formatter.Timestamp(activeEvent.EndDate.ToDateTimeOffset())}   **|**   `/event view {activeEvent.Id}`\n" +
+              $"> `{activeEvent.Description}`\n_ _";
 
         return $"{eventInfo}\n_ _```ansi\n{Formatter.Colorize("Who's around on skribbl?", AnsiColor.Blue)}\n```\n" +
                $"Refreshed: {Formatter.Timestamp(DateTimeOffset.UtcNow)}\nClick to connect: https://www.typo.rip/invite/{serverInvite}";
