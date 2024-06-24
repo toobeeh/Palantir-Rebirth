a1bcaba449926cf24fab5d50277158c53999fcb3
tobeh <tobiasch@gmx.at>
2024-06-24 23:20:33 +0200

    fix sed script

--- tobeh.Palantir.Core/.docker/entrypoint.sh
+++ tobeh.Palantir.Core/.docker/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 sed -i "s!GRPC_URL_SED_PLACEHOLDER!$GRPC_URL!g" /app/Configuration/appsettings.json
-sed -i "s!DISCORD_TOKEN_SED_PLACEHOLDER!$DISCORD_TOKEN!g" /app/Configuration/appsettings.json
+sed -i "s!PALANTIR_DISCORD_TOKEN_SED_PLACEHOLDER!$PALANTIR_DISCORD_TOKEN!g" /app/Configuration/appsettings.json
+sed -i "s!SERVANT_DISCORD_TOKEN_SED_PLACEHOLDER!$SERVANT_DISCORD_TOKEN!g" /app/Configuration/appsettings.json
 sed -i "s!PATREON_TOKEN_SED_PLACEHOLDER!$PATREON_TOKEN!g" /app/Configuration/appsettings.json
 
 # Start the main process
