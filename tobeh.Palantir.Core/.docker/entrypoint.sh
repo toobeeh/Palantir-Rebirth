ebabe4cdaf0e12eb48816cccb2b662a573e35f6a
tobeh <tobiasch@gmx.at>
2024-08-30 01:58:05 +0200

    fix sed order

--- tobeh.Palantir.Core/.docker/entrypoint.sh
+++ tobeh.Palantir.Core/.docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-sed -i "s!GRPC_URL_SED_PLACEHOLDER!$GRPC_URL!g" /app/Configuration/appsettings.json
 sed -i "s!ROLES_GRPC_URL_SED_PLACEHOLDER!$ROLES_GRPC_URL!g" /app/Configuration/appsettings.json
+sed -i "s!GRPC_URL_SED_PLACEHOLDER!$GRPC_URL!g" /app/Configuration/appsettings.json
 sed -i "s!PALANTIR_DISCORD_TOKEN_SED_PLACEHOLDER!$PALANTIR_DISCORD_TOKEN!g" /app/Configuration/appsettings.json
 sed -i "s!SERVANT_DISCORD_TOKEN_SED_PLACEHOLDER!$SERVANT_DISCORD_TOKEN!g" /app/Configuration/appsettings.json
 sed -i "s!PATREON_TOKEN_SED_PLACEHOLDER!$PATREON_TOKEN!g" /app/Configuration/appsettings.json
