81a7f3a8835951ce5f3ea1f0aabed839d7f0a37c
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-02-16 12:33:11 +0100

    fix trigger

--- tobeh.Palantir.Core/Program.cs
+++ tobeh.Palantir.Core/Program.cs
@@ -64,7 +64,6 @@ public class Program
             .AddQuartz(VolatileDataClearerConfiguration.Configure)
             .AddQuartz(DropSchedulerConfiguration.Configure)
             .AddQuartz(RoleUpdaterConfiguration.Configure)
-            .AddQuartz(LeagueEvaluaterConfiguration.Configure)
             .AddQuartzHostedService(options => { options.WaitForJobsToComplete = true; })
             .BuildServiceProvider();
 
